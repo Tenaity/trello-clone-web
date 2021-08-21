605cf27d339ba34c44258f1960b0b6c74230c916
Tenaity <tenaiti.150320@gmail.com>
2021-08-21 20:33:49 +0700

    update architecture

---
+++ src/components/BoardContent/BoardContent.js
@@ -0,0 +1,17 @@
+import Column from 'components/Column/Column'
+import React from 'react'
+
+import './BoardContent.scss'
+
+export default function BoardContent() {
+    return (
+        <div className="board-content">
+          <Column/>
+          <Column/>
+          <Column/>
+          <Column/>
+          <Column/>
+          <Column/>
+      </div>
+    )
+}
