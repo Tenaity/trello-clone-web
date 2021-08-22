e9bc5f626587dfd97e9a2dd032d36d9cf63b9249
Tenaity <tenaiti.150320@gmail.com>
2021-08-22 23:28:19 +0700

    git initial sample data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,17 +1,28 @@
 import Column from 'components/Column/Column'
-import React from 'react'
-
+import React, {useState ,useEffect} from 'react'
+import { initialData } from 'actions/initialData'
+import { isEmpty } from 'lodash'
 import './BoardContent.scss'
+import { mapOrder } from 'utilities/sorts'
 
 export default function BoardContent() {
+  const [board,setBoard] = useState({})
+  const [columns,setColumns] = useState([])
+  useEffect(() => {
+    const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
+    if(boardFromDB) {
+      setBoard(boardFromDB)
+      //sort column
+      
+      setColumns(mapOrder(boardFromDB.columns,boardFromDB.columnOrder, 'id'))
+    }
+  })
+  if(isEmpty(board)){
+    return <div className="not-found" style={{'padding':'10px', 'color':'white'}}>Board not found</div>
+  }
     return (
         <div className="board-content">
-          <Column/>
-          <Column/>
-          <Column/>
-          <Column/>
-          <Column/>
-          <Column/>
-      </div>
+          {columns.map((column,index) => <Column key={index} column={column}/>)}
+        </div>
     )
 }
