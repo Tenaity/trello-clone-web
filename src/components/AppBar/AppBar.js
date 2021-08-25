b6a35913bbc2a1204b7f6c3fb90dbd0cef2b889e
Tenaity <tenaiti.150320@gmail.com>
2021-08-25 21:54:11 +0700

    Implement Drag Drop , Eslint

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -3,7 +3,5 @@ import React from 'react'
 import './AppBar.scss'
 
 export default function AppBar() {
-    return (
-        <nav className="navbar-app">App Bar</nav>
-    )
+  return <nav className="navbar-app">App Bar</nav>
 }
--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,28 +1,54 @@
 import Column from 'components/Column/Column'
-import React, {useState ,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { initialData } from 'actions/initialData'
 import { isEmpty } from 'lodash'
 import './BoardContent.scss'
 import { mapOrder } from 'utilities/sorts'
+import { Container, Draggable } from 'react-smooth-dnd'
 
 export default function BoardContent() {
-  const [board,setBoard] = useState({})
-  const [columns,setColumns] = useState([])
+  const [board, setBoard] = useState({})
+  const [columns, setColumns] = useState([])
   useEffect(() => {
-    const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
-    if(boardFromDB) {
+    const boardFromDB = initialData.boards.find(
+      (board) => board.id === 'board-1'
+    )
+    if (boardFromDB) {
       setBoard(boardFromDB)
       //sort column
-      
-      setColumns(mapOrder(boardFromDB.columns,boardFromDB.columnOrder, 'id'))
+
+      setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
     }
-  })
-  if(isEmpty(board)){
-    return <div className="not-found" style={{'padding':'10px', 'color':'white'}}>Board not found</div>
-  }
+  }, [])
+  if (isEmpty(board)) {
     return (
-        <div className="board-content">
-          {columns.map((column,index) => <Column key={index} column={column}/>)}
-        </div>
+      <div className="not-found" style={{ padding: '10px', color: 'white' }}>
+        Board not found
+      </div>
     )
+  }
+  const onColumnDrop = (dropResult) => {
+    console.log(dropResult)
+  }
+  return (
+    <div className="board-content">
+      <Container
+        orientation="horizontal"
+        onDrop={onColumnDrop}
+        dragHandleSelector=".column-drag-handle"
+        getChildPayload={index => columns[index]}
+        dropPlaceholder={{
+          animationDuration: 150,
+          showOnTop: true,
+          className: 'column-drop-preview'
+        }}
+      >
+        {columns.map((column, index) => (
+          <Draggable key={index}>
+            <Column column={column} />
+          </Draggable>
+        ))}
+      </Container>
+    </div>
+  )
 }
